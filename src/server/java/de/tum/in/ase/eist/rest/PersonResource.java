07a846546e52cbeb1aa2d20b39a3591e4b0c0fae
moritzslz <schultzmoritz@gmail.com>
2023-05-16 00:36:18 +0200

    PersonController & Resource

--- src/server/java/de/tum/in/ase/eist/rest/PersonResource.java
+++ src/server/java/de/tum/in/ase/eist/rest/PersonResource.java
@@ -1,9 +1,15 @@
 package de.tum.in.ase.eist.rest;
 
+import de.tum.in.ase.eist.model.Note;
+import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.service.PersonService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.server.UID;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -16,4 +22,30 @@ public class PersonResource {
     }
 
     // TODO Part 1: Implement the specified endpoints here
+    @PostMapping("persons")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        if (person.getId() == null) {
+            return ResponseEntity.ok(personService.savePerson(person));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("persons/{personID}")
+    public ResponseEntity<Person> updatePersonupdateNote(@RequestBody Person updatedPerson, @PathVariable("personId") UUID personId) {
+        if (updatedPerson.getId().equals(personId)) {
+            return ResponseEntity.ok(personService.savePerson(updatedPerson));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("persons/{personID}")
+    public ResponseEntity<Void> deletePerson(@PathVariable("personId") UUID personId) {
+        personService.deletePerson(personId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("persons")
+    public ResponseEntity<List<Note>> getAllPersons() {
+        return ResponseEntity(personService.getAllPersons());
+    }
 }
